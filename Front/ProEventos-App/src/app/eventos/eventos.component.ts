d4dd840f0e337eb6c3da111b17482568a9c002f4
goad <gustavo.brandao@gft.com>
2022-03-16 14:21:42 -0300

    Atualização do Front

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  larguraImg =  150;
+  margemImg =  2;
+  mostrarImg = false;
+  private _filtroLista = '';
+
+  public get filtroLista() {
+    return this._filtroLista;
+  }
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -18,9 +31,24 @@ export class EventosComponent implements OnInit {
 
   public getEventos (): void {
     this.http.get('https://localhost:5001/api/Evento').subscribe(
-      reponse => this.eventos = reponse,
+      reponse =>
+      {
+        this.eventos = reponse;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
-  }
+    }
 
-}
+    alterarImg () {
+      this.mostrarImg = !this.mostrarImg;
+    }
+
+    filtrarEventos( filtarPor: string): any {
+      filtarPor = filtarPor.toLocaleLowerCase();
+      return this.eventos.filter(
+        (evento: {tema: string; local: string} )=> evento.tema.toLocaleLowerCase().indexOf(filtarPor) !== -1
+        || evento.local.toLocaleLowerCase().indexOf(filtarPor) !== -1
+      )
+    }
+  }
